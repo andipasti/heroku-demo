3d5d73b8c3d6d1414ab9a0b0c0720894efcbdbe8
andipasti <philippe.nahlik@gmail.com>
2021-11-09 17:18:59 +0100

    server.js bugfix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require ('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(_dirname + '/dist/website'));
+app.use(express.static(__dirname + '/dist/website'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/website/index.html'));
 });
