c8dee32adb3b48fc96b97325e1c68b7da496c79e
andipasti <philippe.nahlik@gmail.com>
2021-11-09 17:27:01 +0100

    server.js bugfix2

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require ('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/website'));
+app.use(express.static(__dirname + '/dist/phils-heroku-demo-app'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/website/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/phils-heroku-demo-app/index.html'));
 });
 app.listen(process.env.PORT || 8080);
